e4cbaa28c3329ddb3f59465933ea2f02639f70f9
Yuvraj Singh <yuvrajsingh@Admins-MacBook-Air.local>
2025-01-08 13:50:43 +0530

    Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a # character.

--- chessboard.js
+++ chessboard.js
@@ -1,13 +1,14 @@
 function printChessBoard(n){
     let chessBoard = "";
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    for(let i=0; i<n; i++){
+        for(let j=0; j<n; j++){
             chessBoard+= (i+j)%2===0? '#': ' '; 
  
         }
         chessBoard+='\n';
     }
     console.log(chessBoard);
+    
 }
 
 printChessBoard(8); 
